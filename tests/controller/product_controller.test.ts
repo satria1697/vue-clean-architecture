87d9f6f76f872a6368dc15857c1de4e3b7e0997e
satria1697 <andhikasatriab@gmail.com>
2023-01-23 08:23:04 +0700

    [fix] fix unit test

--- tests/controller/product_controller.test.ts
+++ tests/controller/product_controller.test.ts
@@ -5,25 +5,20 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { mock } from 'vitest-mock-extended'
 
 import { useProduct } from '../../src/controller/productController'
-import regisContainer from '../../src/di/registration'
-import { ApiInterface } from '../../src/interfaces/services/apiInterface'
-import { ProductRepository } from '../../src/repositories/productRepository'
+import { ProductRepositoryInterface } from '../../src/interfaces/repositories/productRepositoryInterface'
 import { getProductResponse } from '../helper/product'
 
 describe('product_controller', () => {
-  regisContainer()
-  const api = mock<ApiInterface>()
-  const productRepository = new ProductRepository(api)
-  const getProductData = vi.spyOn(productRepository, 'getProductData')
+  const productRepository = mock<ProductRepositoryInterface>()
   const resolve = vi.spyOn(container, 'resolve')
-  resolve.mockReturnValue(productRepository)
 
   beforeEach(() => {
     setActivePinia(createPinia())
+    resolve.mockReturnValueOnce(productRepository)
   })
 
   it('get data', async () => {
-    getProductData.mockResolvedValue(getProductResponse())
+    productRepository.getProductData.mockResolvedValue(getProductResponse())
     const productStore = useProduct()
     expect(productStore.isLoading).toBeTruthy()
     expect(productStore.productLength).toBe(0)
@@ -35,7 +30,7 @@ describe('product_controller', () => {
   })
 
   it('get empty data', async () => {
-    getProductData.mockResolvedValue(getProductResponse(true))
+    productRepository.getProductData.mockResolvedValue(getProductResponse(true))
     const productStore = useProduct()
     expect(productStore.isLoading).toBeTruthy()
     expect(productStore.productLength).toBe(0)
